fdf9d312ca146c1a403fbc2049da5dc7d1ae6b3f
amemiyayuko <kyd_dong@hotmail.com>
2017-09-27 20:35:46 -0400

    built a simple network for experiments

--- cifar10.py
+++ cifar10.py
@@ -1,8 +1,9 @@
-import cv2
+import os
+
 import numpy as np
 
 
-def unpickle(file=r"C:\Datasets\cifar10\data_batch_1"):
+def unpickle(file):
     import pickle
     with open(file, 'rb') as fo:
         dict = pickle.load(fo, encoding='bytes')
@@ -20,12 +21,19 @@ def load_all_images(files):
             channel_r = img[:32 * 32]
             channel_g = img[32 * 32:2 * 32 * 32]
             channel_b = img[2 * 32 * 32:]
-            output_img = np.zeros([32, 32, 3], dtype=np.uint8)
+            output_img = np.zeros([32, 32, 3], dtype=np.float32)
             output_img[:, :, 0] = channel_r.reshape([32, 32])
             output_img[:, :, 1] = channel_g.reshape([32, 32])
             output_img[:, :, 2] = channel_b.reshape([32, 32])
             all_imgs.append(output_img)
-            all_labels.append(d[b"label"][i])
-            cv2.imshow("test", output_img)
-            print(d[b"label"][i])
-            cv2.waitKey()
+            all_labels.append(d[b"labels"][i])
+    return np.asarray(all_imgs, dtype=np.float32), np.asarray(all_labels, dtype=np.float32)
+
+
+def load_cifar10(prefix):
+    train_images, train_labels = load_all_images(
+        [os.path.join(prefix, r"data_batch_1"), os.path.join(prefix, r"data_batch_2"),
+         os.path.join(prefix, r"data_batch_3"), os.path.join(prefix, r"data_batch_4"),
+         os.path.join(prefix, r"data_batch_5")])
+    test_images, test_labels = load_all_images([os.path.join(prefix, r"test_batch")])
+    return train_images, train_labels, test_images, test_labels
---
+++ train.py
@@ -0,0 +1,49 @@
+import numpy as np
+import tensorflow as tf
+
+from cifar10 import load_cifar10
+from model import model
+
+
+def preprocessing():
+    train_images, train_labels, test_images, test_labels = load_cifar10(
+        prefix=r"/home/qide/Dataset/cifar-10-batches-py/")
+    train_images /= 255.0
+    test_images /= 255.0
+    train_labels = (np.arange(10) == train_labels[:, None]).astype(np.float32)
+    test_labels = (np.arange(10) == test_labels[:, None]).astype(np.float32)
+    return train_images, train_labels, test_images, test_labels
+
+
+def unison_shuffled_copies(a, b):
+    assert len(a) == len(b)
+    p = np.random.permutation(len(a))
+    return a[p], b[p]
+
+
+def train(batch_size, max_epoch):
+    train_images, train_labels, test_images, test_labels = preprocessing()
+    with tf.Session() as sess:
+        graph = model()
+        saver = tf.train.Saver()
+        trn_summary = tf.summary.FileWriter('logger/', sess.graph)
+        sess.run(tf.global_variables_initializer())
+        for i in range(max_epoch):
+            if (i % 10 == 0):
+                saver.save(sess, "ckpt/", global_step=graph['global_step'])
+            # train_images, train_labels=unison_shuffled_copies(train_images,train_labels)
+            for j in range(len(train_images) // batch_size):
+                trn_imgs = train_images[j * batch_size:(j + 1) * batch_size]
+                trn_labels = train_labels[j * batch_size:(j + 1) * batch_size]
+                graph_dict = {graph["x"]: trn_imgs,
+                              graph["y"]: trn_labels,
+                              graph["keep_prob"]: 0.5}
+                opt, loss, summary, step = sess.run(
+                    [graph['optimizer'], graph['loss'], graph['summary'], graph['global_step']], feed_dict=graph_dict)
+                trn_summary.add_summary(summary, step)
+                print("# Epoch " + str(i) + " Step " + str(step) + " with loss " + str(loss))
+        sess.close()
+
+
+if __name__ == "__main__":
+    train(512, 2000)
