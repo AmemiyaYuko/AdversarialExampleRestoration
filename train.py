d63b97548ccd600d4b88c5331353e5653d2b51db
amemiyayuko <kyd_dong@hotmail.com>
2017-09-29 13:32:48 -0400

    Added test function

--- train.py
+++ train.py
@@ -26,11 +26,13 @@ def train(batch_size, max_epoch):
     with tf.Session() as sess:
         graph = model()
         saver = tf.train.Saver()
-        trn_summary = tf.summary.FileWriter('logger/', sess.graph)
+        trn_summary = tf.summary.FileWriter('logger/train/', sess.graph)
+        tst_summary = tf.summary.FileWriter('logger/test/')
         sess.run(tf.global_variables_initializer())
         for i in range(max_epoch):
             if (i % 10 == 0):
                 saver.save(sess, "ckpt/", global_step=graph['global_step'])
+            gstep = 0
             # train_images, train_labels=unison_shuffled_copies(train_images,train_labels)
             for j in range(len(train_images) // batch_size):
                 trn_imgs = train_images[j * batch_size:(j + 1) * batch_size]
@@ -41,9 +43,19 @@ def train(batch_size, max_epoch):
                 opt, loss, summary, step = sess.run(
                     [graph['optimizer'], graph['loss'], graph['summary'], graph['global_step']], feed_dict=graph_dict)
                 trn_summary.add_summary(summary, step)
+                gstep = step
                 print("# Epoch " + str(i) + " Step " + str(step) + " with loss " + str(loss))
-        sess.close()
+
+            # test
+            graph_dict = {graph["x"]: test_images[:1000],
+                          graph["y"]: test_labels[:1000],
+                          graph["keep_prob"]: 1}
+            accuracy, summary = sess.run([graph['accuracy'], graph['summary']], feed_dict=graph_dict)
+            tst_summary.add_summary(summary, global_step=gstep)
+            print("Accuracy in epoch #" + str(i) + " = " + str(accuracy))
+
+    sess.close()
 
 
 if __name__ == "__main__":
-    train(512, 2000)
+    train(512, 500)
