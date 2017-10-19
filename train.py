98f3778e430ebc9a71de984b97bca74c958ae9fb
amemiyayuko <kyd_dong@hotmail.com>
2017-10-19 17:52:44 -0400

    Add whitten

--- train.py
+++ train.py
@@ -15,7 +15,7 @@ def _preprocess_dataset(ori, adv):
     adv_decoded = tf.image.decode_jpeg(adv_str, channels=3)
     ori_resized = tf.image.resize_images(ori_decoded, [image_size, image_size])
     adv_resized = tf.image.resize_images(adv_decoded, [image_size, image_size])
-    return ori_resized, adv_resized
+    return ori_resized / 255 * 2 - 1, adv_resized / 255 * 2 - 1
 
 
 def get_database(adv_path, ori_path):
@@ -34,7 +34,7 @@ def get_database(adv_path, ori_path):
     dataset = dataset.map(_preprocess_dataset)
     dataset = dataset.repeat()
     dataset = dataset.batch(FLAGS.batch_size)
-    dataset = dataset.shuffle(buffer_size=1000)
+    dataset = dataset.shuffle(buffer_size=FLAGS.batch_size)
     iterator = dataset.make_one_shot_iterator()
     return iterator
 
@@ -43,8 +43,7 @@ def main(_):
     sess = tf.Session()
     sess.run(tf.global_variables_initializer())
     iterator = get_database(r"/home/qide/imagenet-data/raw-data/validation", r"/home/qide/output")
-    print(sess.run(iterator.get_next()))
-
+    ori, adv = sess.run(iterator.get_next())
 
 if __name__ == "__main__":
     tf.app.run()
