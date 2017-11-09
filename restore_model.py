d3d7300b07488dab69afd23602cd4fa6fe0300da
AmemiyaYuko <kyd_dong@hotmail.com>
2017-11-09 00:39:16 -0500

    Adjust hyperparameters

--- restore_model.py
+++ restore_model.py
@@ -3,7 +3,7 @@ from tensorflow.contrib.layers import l2_regularizer
 
 
 # using tf 1.4.0 rc
-def model(ori_image, adv_image, num_layers=16, image_size=299, is_trainging=False):
+def model(ori_image, adv_image, num_layers=12, image_size=299, is_trainging=False):
     residual_image = tf.add(ori_image, -1.0 * adv_image)
     x = adv_image  # tf.layers.conv2d(inputs=tf.layers.batch_normalization(adv_image, training=is_trainging), filters=32,
     # kernel_size=[1, 1], padding="valid")
@@ -12,15 +12,15 @@ def model(ori_image, adv_image, num_layers=16, image_size=299, is_trainging=Fals
     for i in range(num_layers // 2):
         x = tf.layers.batch_normalization(nodes[-1], training=is_trainging)
         x = tf.nn.leaky_relu(x, name="lrelu_conv_%d" % i, alpha=0.2)
-        x = tf.layers.conv2d(inputs=x, filters=64, kernel_size=[3, 3], padding="valid",
+        x = tf.layers.conv2d(inputs=x, filters=20, kernel_size=[3, 3], padding="valid",
                              name="conv_%d" % i, kernel_regularizer=l2_regularizer(0.4))
         nodes.append(x)
     x = nodes[-1]
     for i in range(num_layers // 2):
-        x = tf.add(x, nodes[num_layers // 2 - i])
+        x = tf.add(x, nodes[num_layers // 2 - i]) / 2.0
         x = tf.layers.batch_normalization(x, training=is_trainging)
         x = tf.nn.leaky_relu(x, name="lrelu_deconv_%d" % i, alpha=0.2)
-        x = tf.layers.conv2d_transpose(inputs=x, filters=64, kernel_size=[3, 3], padding="valid",
+        x = tf.layers.conv2d_transpose(inputs=x, filters=20, kernel_size=[3, 3], padding="valid",
                                        name="deconv_%d" % i, kernel_regularizer=l2_regularizer(0.4))
 
     output = tf.layers.conv2d_transpose(inputs=x, filters=3, kernel_size=[1, 1], padding="valid",
